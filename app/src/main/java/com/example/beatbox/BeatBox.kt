cc7e3495cb0da947f8bd8102fefd594431d5661d
Stanislav.Kolontsev <stas.koloncev232@gmail.com>
2024-08-27 23:48:22 +0200

    addassets and BeatBoxkt

---
+++ app/src/main/java/com/example/beatbox/BeatBox.kt
@@ -0,0 +1,11 @@
+package com.example.beatbox
+
+import android.content.res.AssetManager
+
+private const val TAG = "BeatBox"
+private const val SOUNDS_FOLDER = "sample_sounds"
+
+class BeatBox(private val assets: AssetManager) {
+
+
+}
